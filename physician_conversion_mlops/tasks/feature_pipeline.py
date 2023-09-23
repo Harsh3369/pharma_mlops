e15005765cdbd140d11e5629d042b22070df4a3b
Harsh3369 <harshitsundriyal@gmail.com>
2023-09-23 11:18:17 +0530

    updated_feature_pipeline

--- physician_conversion_mlops/tasks/feature_pipeline.py
+++ physician_conversion_mlops/tasks/feature_pipeline.py
@@ -4,6 +4,9 @@ import numpy as np
 
 
 from sklearn.preprocessing import OrdinalEncoder, OneHotEncoder
+from sklearn.feature_selection import VarianceThreshold
+from sklearn.feature_selection import SelectKBest
+from sklearn.feature_selection import f_classif
 import warnings
 import os
 import boto3
@@ -36,9 +39,57 @@ class DataPrep(Task):
                 df_input = utils.load_data_from_s3(self)
 
                 df_input = df_input.reset_index()
-        
+
+                #Clean column names
+                df_input.columns = df_input.columns.str.strip()
+                df_input.columns = df_input.columns.str.replace(' ', '_')
+
+                #Drop unwanted column: "HCO Affiliation" - "Affiliation Type" is more valid column for us
+                drop_col_list = self.conf['feature_transformation']['drop_column_list']
+                df_input.drop(drop_col_list, axis= 1, inplace= True)
+
+                #One hot encode categorical features
+                encode_col_list = self.conf['feature_transformation']['one_hot_encode_feature_list']
+                df_input = pd.get_dummies(df_input, columns=encode_col_list, drop_first=True)
+
+                #Select variables for feature selection
+                id_target_col_list = self.conf['feature_transformation']['id_target_col_list']
+                col_for_feature_selection = df_input.columns.difference(id_target_col_list)
+
+                #Variance threshold feature selection method
+                threshold = self.conf['param_values']['variance_threshold_value']
+                var_thr = VarianceThreshold(threshold = threshold) #Removing both constant and quasi-constant
+                var_thr.fit(df_input[col_for_feature_selection])
+                #var_thr.get_support()
+
+                df_input_subset = df_input[col_for_feature_selection]
+                remove_col_list = [col for col in df_input_subset.columns 
+                                if col not in df_input_subset.columns[var_thr.get_support()]]
                 
-                push_status = utils.push_df_to_s3(self,df_input)
+                #remove above list column from master dataframe
+                df_input.drop(remove_col_list, axis = 1, inplace = True, errors= 'ignore')
+
+                #Feature Selection Using Select K Best
+                n = self.conf['param_values']['select_k_best_feature_num']
+                id_col_list = ['NPI_ID', 'HCP_ID']
+                target_col = self.conf['feature_transformation']['target_col']
+                
+                top_n_col_list = utils.select_kbest_features(
+                      df_input.drop(id_col_list,axis=1),target_col, n)
+                
+                #Convert to list
+                top_n_col_list = top_n_col_list.tolist()
+
+                # Dump top_n_col_list to s3 bucket
+                pickle_file_path = self.conf['preprocessed']['pickle_file_dump_list']
+                utils.pickle_dump(self,top_n_col_list,pickle_file_path)
+                
+                #column list for dataframe
+                cols_for_model_df_list = id_col_list + top_n_col_list
+                df_feature_eng_output = df_input[cols_for_model_df_list]
+                df_model_input = df_feature_eng_output.copy()
+                
+                push_status = utils.push_df_to_s3(self,df_model_input)
                 print(push_status)
 
                 
--- physician_conversion_mlops/utils.py
+++ physician_conversion_mlops/utils.py
@@ -2,9 +2,12 @@
 import boto3
 import urllib
 import pandas as pd
+from sklearn.feature_selection import SelectKBest
+from sklearn.feature_selection import f_classif
 
 from io import BytesIO
 import uuid
+import pickle
 
 
 #useful functions
@@ -84,5 +87,48 @@ class utils(Task):
 
         return df_input
     
+
+    def select_kbest_features(df, target_col,n):
+        """
+        Selects the top n features from the DataFrame using the SelectKBest algorithm.
+
+        Args:
+            df: The DataFrame to select features from.
+            n: The number of features to select.
+
+        Returns:
+            A list of the top n features.
+        """
+
+
+        selector = SelectKBest(k=n)
+        selected_features = selector.fit_transform(df, target_col)
+        
+        mask = selector.get_support()
+        top_n_features = df.columns[mask]
+
+        return top_n_features
         
         
+    def pickle_dump(self,list_columns,path):
+        with open(path,"wb") as file:
+            pickle.dump(list_columns,file)
+        aws_region = self.conf['s3']['aws_region']
+        bucket_name = self.conf['s3']['bucket_name']
+        file_path = self.conf['s3']['file_path']
+
+        spark = SparkSession.builder.appName("CSV Loading Example").getOrCreate()
+
+        dbutils = DBUtils(spark)
+
+        aws_access_key = dbutils.secrets.get(scope="secrets-scope", key="aws-access-key")
+        aws_secret_key = dbutils.secrets.get(scope="secrets-scope", key="aws-secret-key")
+
+        s3 = boto3.resource("s3",aws_access_key_id=aws_access_key, 
+                    aws_secret_access_key=aws_secret_key, 
+                    region_name=aws_region)
+
+        pickled_data = pickle.dumps(path)
+        s3_object_key = self.conf['preprocessed']['pickle_file_dump_list'] 
+        s3.Object(self.conf['s3']['bucket_name'], s3_object_key).put(Body=pickled_data)
+
